32583ed12f15db2761fc1e11778abdbe26c26da8
Karan Telgad <karantelgad4@gmail.com>
2024-12-24 11:56:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ require("dotenv").config();
 require("./models/db");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://login-app-ui-wine.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true, 
+}));
 
 const PORT = process.env.PORT || 5500;
 
